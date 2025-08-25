e72d93f776a27b3e9052dd77c5fab6a93da80824
zaherarnaout <zharnaout@hotmail.com>
2025-08-25 15:47:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5945,7 +5945,7 @@ function initializeFooterContactButton() {
 function initializeFooterLogoHomeButton() {
   console.log('🏠 Initializing footer logo home button...');
   
-  const footerLogo = document.querySelector('.footer-section .image-35, #footer-logo, .footer-logo');
+  const footerLogo = document.querySelector('.middle-footer-wrapper .image-35, #footer-logo, .footer-logo');
   
   if (!footerLogo) {
     console.log('⚠️ Footer logo not found');
